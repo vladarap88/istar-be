6a72cf4aeabfe5cb1014fc7c4387ddae8bc930e7
vlada <vladarap88@gmail.com>
2025-01-28 21:22:39 -0800

    add allowed host

--- istar_be/settings.py
+++ istar_be/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-$)0cmhs)a@xs1+dgj4w2*jfd24ns@=98wf@hsv-krs4tww(x2-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["istar-be.onrender.com"]
 
 
 # Application definition
